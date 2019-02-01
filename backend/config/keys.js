f1c93e2a8c206b1ce426b2036390548f205767eb
wotj08090@gmail.com <wotj08090@gmail.com>
2019-02-01 20:34:26 +0900

    key.js 삭제

--- backend/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://localhost:27017/mernDemo',
-
-  // About MailGun Account
-  MAILGUN_USER: 'postmaster@sandbox7f69cc8dd13e4230a0db2803422b191e.mailgun.org',
-  MAILGUN_PASS: '6409f3e4f0935ef6c8afafb8474f13cb-c8c889c9-89d378b5'
-}
